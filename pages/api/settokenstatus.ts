4d512eadb0b11ebf026736c872f8ab14f3de767a
Codegnosis <paul@codegnosis.co.uk>
2024-06-26 15:35:34 +0100

    only cascade non-verified token status to associated pairs

--- pages/api/settokenstatus.ts
+++ pages/api/settokenstatus.ts
@@ -41,50 +41,9 @@ export default async function handler(
         where: { id: tokenId },
         data: { status: newStatus, verificationMethod: "manual" },
     })
-
-    if(newStatus === 1) {
-        // check for duplicate token symbols. Assume this is the correct token, set others to "2"
-        // and cascade through pairs
-
-        // first get possible duplicate token symbols
-        const duplicateTokens = await prisma.token.findMany({
-            where: {
-                symbol: token.symbol,
-                chain: token.chain,
-            }
-        })
-
-        for(let i = 0; i < duplicateTokens.length; i += 1) {
-            let dt = duplicateTokens[i]
-            if(dt.id === tokenId) {
-                continue
-            }
-            dt = await prisma.token.update({
-                where: { id: dt.id },
-                data: { status: 2, verificationMethod: "cascade" },
-            })
-            updatedTokenCount += 1
-
-            // pairs using this token
-            const updatedPairCountRes = await prisma.pair.updateMany({
-                where: {
-                    OR: [
-                        {
-                            token0Id: dt.id
-                        },
-                        {
-                            token1Id: dt.id
-                        },
-                    ],
-                },
-                data: { status: 2, verificationMethod: "cascade" },
-            })
-
-            updatedPairCount += updatedPairCountRes.count
-        }
-
-    } else {
-        // status is something other than 1 (verified). Cascade through associated pairs
+    
+    if(newStatus !== 1) {
+        // cascade update associated pairs
         const updatedPairCountRes = await prisma.pair.updateMany({
             where: {
                 OR: [
@@ -101,6 +60,66 @@ export default async function handler(
 
         updatedPairCount = updatedPairCountRes.count
     }
+
+    // if(newStatus === 1) {
+    //     // check for duplicate token symbols. Assume this is the correct token, set others to "2"
+    //     // and cascade through pairs
+    //
+    //     // first get possible duplicate token symbols
+    //     const duplicateTokens = await prisma.token.findMany({
+    //         where: {
+    //             symbol: token.symbol,
+    //             chain: token.chain,
+    //         }
+    //     })
+    //
+    //     for(let i = 0; i < duplicateTokens.length; i += 1) {
+    //         let dt = duplicateTokens[i]
+    //         if(dt.id === tokenId) {
+    //             continue
+    //         }
+    //         dt = await prisma.token.update({
+    //             where: { id: dt.id },
+    //             data: { status: 2, verificationMethod: "cascade" },
+    //         })
+    //         updatedTokenCount += 1
+    //
+    //         // pairs using this token
+    //         const updatedPairCountRes = await prisma.pair.updateMany({
+    //             where: {
+    //                 OR: [
+    //                     {
+    //                         token0Id: dt.id
+    //                     },
+    //                     {
+    //                         token1Id: dt.id
+    //                     },
+    //                 ],
+    //             },
+    //             data: { status: 2, verificationMethod: "cascade" },
+    //         })
+    //
+    //         updatedPairCount += updatedPairCountRes.count
+    //     }
+    //
+    // } else {
+    //     // status is something other than 1 (verified). Cascade through associated pairs
+    //     const updatedPairCountRes = await prisma.pair.updateMany({
+    //         where: {
+    //             OR: [
+    //                 {
+    //                     token0Id: tokenId
+    //                 },
+    //                 {
+    //                     token1Id: tokenId
+    //                 },
+    //             ],
+    //         },
+    //         data: { status: newStatus, verificationMethod: "cascade" },
+    //     })
+    //
+    //     updatedPairCount = updatedPairCountRes.count
+    // }
 
     return res.status(200).json({ success: true, data: {new_status: token.status, id: token.id, affected_pairs: updatedPairCount, affected_tokens: updatedTokenCount } })
 
