ca56db0a6d877821357851382e7d33a47cff205d
Codegnosis <paul@codegnosis.co.uk>
2024-06-28 13:42:59 +0100

    fetch improvements and loader

--- pages/api/getprices.ts
+++ pages/api/getprices.ts
@@ -14,6 +14,7 @@ export default async function handler(
         prices: [],
         chain: "",
         dex: "",
+        addresses: "",
         error: "",
         success: false,
     }
@@ -36,6 +37,7 @@ export default async function handler(
     const addrStr = `"${addresses.join('","')}"`
     retData.chain = chain
     retData.dex = dex
+    retData.addresses = addrStr
 
     let poolsName = null
     let url = null
